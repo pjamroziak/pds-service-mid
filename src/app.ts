f8495890e9da0ef8546992e6e1b397aa27deabc0
Pawel Jamroziak <pjamroziak@gmail.com>
2021-03-17 10:24:13 -0700

    feat: Use CommandsRouter

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
+import { commandsRouter } from "./routes/v1/commands.route";
 
 dotenv.config();
 
 const app = express();
+const appBaseUrl: string = process.env.EXPRESS_ROUTE_BASE  + "/" + process.env.EXPRESS_ROUTE_VERSION;
+
+app.use(express.json());
+app.use(appBaseUrl, commandsRouter);
 
 app.listen(process.env.EXPRESS_PORT, () => {
     console.log('App listen on ' + process.env.EXPRESS_PORT);
-})
+});
