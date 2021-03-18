17a97e526fcd482d7fa2dfbe48318b9194c37909
Pawel Jamroziak <pjamroziak@gmail.com>
2021-03-18 13:25:19 -0700

    feat: Recreate commands POST
    New http requester, model converter and message broker producer

--- src/routes/v1/commands.route.ts
+++ src/routes/v1/commands.route.ts
@@ -1,16 +1,32 @@
 import express, { Response } from "express";
 import { createValidator, ValidatedRequest } from "express-joi-validation";
-import { getRunCmdObjectSchema, RunCmdRequestSchema } from "./schemas/runCmdRequestSchema.model";
+
+import { CommandsController } from "../../controllers/commands.controller";
+import { PersonalDataModel } from "../../models/personalData.model";
+import { getRunCmdObjectSchema, RunCmdModel, RunCmdRequestSchema } from "../../models/runCmdRequest.model";
+import { TpaModel } from "../../models/tpaGorest.model";
 
 export const commandsRouter = express.Router();
 
 const validator = createValidator();
+const commandController = new CommandsController();
 
 commandsRouter.post(
     "/commands/run", 
-    validator.body(getRunCmdObjectSchema()),
+    validator.body(getRunCmdObjectSchema),
     async (req: ValidatedRequest<RunCmdRequestSchema>, res: Response) => {
-
-        res.status(201).send();
+        const requestBody: RunCmdModel = req.body as RunCmdModel;
+        try {
+            const tpaModel: TpaModel = await commandController.fetchTpaModel(requestBody);
+            const personalDataModel: PersonalDataModel = await commandController.convertTpaModel(tpaModel);
+    
+            const isTaskPublished: boolean = await commandController.publishTask(personalDataModel);
+            
+            const responseStatusCode: number = isTaskPublished ? 201 : 403;
+            res.status(responseStatusCode).send();
+        }
+        catch(error) {
+            res.status(500).send("Server Error");
+        }
     }
 )
